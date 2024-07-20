ef145c9635abb1080005b3dc3608fbe86366c9d9
cppNut <mj9425326498@gmail.com>
2024-07-20 19:21:15 +0530

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-
+import dotenv from 'dotenv';
 //components
 import Connection from './database/db.js';
 import Router from './routes/route.js';
@@ -18,10 +17,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', Router);
 
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
 Connection(username, password);
 
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+
